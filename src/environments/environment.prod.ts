e7ac585dcca07d9305fdcb2371372c3ce8a36b6a
Gabriel Lacerda Oliveira Penna <gabriel_600270@trt5.jus.br>
2023-02-21 17:33:15 -0300

    !cors

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const environment = {
 	production: true,
 	envName: 'prod',
 	baseUrl: 'https://siach-web-production.up.railway.app/',
-	apiUrl: 'https://siach-web-production.up.railway.app/https://siach-api-production.up.railway.app/',
+	apiUrl: 'https://siach-api-production.up.railway.app/',
 	apiUrlSwitch: 'http://localhost:8080/',
 };
